7c1ed34d01419edc8f4e0134fbf03fee270acbd6
tomporvaz <tomporvaz@gmail.com>
2019-07-23 06:55:48 -0400

    Added strictEqual and notStrictEqual to challenge 6.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -50,17 +50,17 @@ suite('Unit Tests', function(){
     // .equal() compares objects using '=='
     test('#equal, #notEqual', function(){
       assert.equal( 12, '12', 'numbers are coerced into strings with == ');
-      assert.equal( {value: 1}, {value:1}, '== compares object references');
+      assert.notEqual( {value: 1}, {value:1}, '== compares object references');
       assert.equal( 6 * '2', '12', 'no more hints...');
       assert.notEqual( 6 + '2', '12', 'type your error message if you want' );
     });
     /** 6 - .strictEqual(), .notStrictEqual() **/
     // .strictEqual() compares objects using '==='
     test('#strictEqual, #notStrictEqual', function(){
-      assert.fail( 6, '6' );
-      assert.fail( 6, 3*2 );
-      assert.fail( 6 * '2', 12 );
-      assert.fail( [1, 'a', {} ], [1, 'a', {}] );
+      assert.strictEqual( 6, '6' );
+      assert.strictEqual( 6, 3*2 );
+      assert.strictEqual( 6 * '2', 12 );
+      assert.notStrictEqual( [1, 'a', {} ], [1, 'a', {}] );
     });
     /** 7 - .deepEqual(), .notDeepEqual() **/
     // .deepEqual() asserts that two object are deep equal
