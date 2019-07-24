950bfdc9c3b2cc1232c472e125adc4d6066929db
tomporvaz <tomporvaz@gmail.com>
2019-07-24 07:51:58 -0400

    Added isArray and isNot Array for challenge 11, and add include and notInclude for challenge 12.

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -103,7 +103,7 @@ suite('Unit Tests', function(){
     // it should be less than 1
     test('#approximately', function() {
       assert.approximately(weirdNumbers(0.5) , 1, 0.5);
-      assert.approximately(weirdNumbers(0.2) , 1, 0.2);
+      assert.approximately(weirdNumbers(0.2) , 1, 0.8);
     });
   });
 
@@ -116,14 +116,14 @@ suite('Unit Tests', function(){
     
     /** 11 - #isArray vs #isNotArray **/
     test('#isArray, #isNotArray', function() {
-      assert.fail('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
-      assert.fail([1,2,3].indexOf(2), 'indexOf returns a number.');
+      assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an Array');
+      assert.isNotArray([1,2,3].indexOf(2), 'indexOf returns a number.');
     });
     
     /** 12 - #include vs #notInclude **/
     test('Array #include, #notInclude', function() {
-      assert.fail(winterMonths, 'jul', "It's summer in july...");
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language !!');
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
+      assert.include(backendLanguages, 'javascript', 'JS is a backend language !!');
     });
   });
 
