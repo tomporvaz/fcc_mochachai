958c26ae16b9b48db41af0f315ceaa0215dcafc2
tomporvaz <tomporvaz@gmail.com>
2019-07-27 19:45:41 -0400

    Changed double quotes to single quotes in challenge 22.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -141,9 +141,9 @@ suite('Functional Tests', function() {
         .send({surname: "da Verrazzano"})
         .end(function(err, res){
           assert.equal(res.status, 200);
-          assert.equal(res.type, "application/json");
-          assert.equal(res.body.name, "Giovanni");
-          assert.equal(res.body.surname, "da Verrazzano")
+          assert.equal(res.type, 'application/json');
+          assert.equal(res.body.name, 'Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano')
           done();
         })
         
