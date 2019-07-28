174259acfcdb22f3ccf9210fb486384d3107fa78
tomporvaz <tomporvaz@gmail.com>
2019-07-27 21:08:09 -0400

    added tests for 24.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -263,14 +263,20 @@ suite('Functional Tests', function() {
       
       /** Try it again... No help this time **/
       test('submit "surname" : "Vespucci" - write your e2e test...', function(done) {
+        browser.fill('surname', 'Vespucci')
+        .pressButton('submit', function(){
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.element('span#date', 1);
+          done();
+        })
 
         // fill the form, and submit.
         // assert that status is OK 200
         // assert that the text inside the element 'span#name' is 'Amerigo'
         // assert that the text inside the element 'span#surname' is 'Vespucci'
         // assert that the element(s) 'span#dates' exist and their count is 1
-        assert.fail();
-        done();
       
       });
     });
